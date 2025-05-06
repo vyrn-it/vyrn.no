ed97c3d91dc9bdb96f973c057df6ce1247e2f12c
dravndal <danielcravndal@gmail.com>
2025-05-07 00:26:39 +0200

    fix(layout): extract common logic to app layer

--- src/app.tsx
+++ src/app.tsx
@@ -3,18 +3,22 @@ import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import Nav from "~/components/Nav";
 import "./app.css";
+import Footer from "./components/Footer";
 
 export default function App() {
-  return (
-    <Router
-      root={props => (
-        <>
-          <Nav />
-          <Suspense>{props.children}</Suspense>
-        </>
-      )}
-    >
-      <FileRoutes />
-    </Router>
-  );
+	return (
+		<Router
+			root={props => (
+				<>
+					<div class="min-h-screen bg-gray-900 text-gray-100">
+						<Nav />
+						<Suspense>{props.children}</Suspense>
+						<Footer />
+					</div>
+				</>
+			)}
+		>
+			<FileRoutes />
+		</Router>
+	);
 }
